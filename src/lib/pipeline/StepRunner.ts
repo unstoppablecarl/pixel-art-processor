fc03ac1550b3ab5f595f5cbfb034be8bb41f63ac
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-12-29 14:03:53 -0500

    working4

--- src/lib/pipeline/StepRunner.ts
+++ src/lib/pipeline/StepRunner.ts
@@ -1,7 +1,7 @@
 import type { StepDataTypeInstance } from '../../steps.ts'
 import { StepValidationError } from '../errors.ts'
 import { copyStepDataOrNull } from '../step-data-types/_step-data-type-helpers.ts'
-import type { MaybePromise, Narrows } from '../util/misc.ts'
+import type { MaybePromise } from '../util/misc.ts'
 import type { AnyStep, AnyStepContext } from './Step.ts'
 
 export type PreviewOutput = ImageData | ImageData[] | null
@@ -10,10 +10,12 @@ export type StepRunner<T extends AnyStepContext> =
   | NormalStepRunner<T>
   | ForkStepRunner<T>
 
-export type StepRunnerResult = {
+type BaseRunnerResult = {
   preview: AnyStep['outputPreview'],
-  output: AnyStep['outputData'],
   validationErrors: AnyStep['validationErrors']
+}
+export type StepRunnerResult = BaseRunnerResult & {
+  output: AnyStep['outputData']
 }
 
 export type NormalStepRunner<T extends AnyStepContext> = ({ config, inputData }: {
@@ -32,17 +34,15 @@ export type NormalStepRunnerOutput<Output> =
   validationErrors?: StepValidationError[]
 }
 
-export type NormalStepRunnerResult<Output extends StepDataTypeInstance> = Narrows<StepRunnerResult, {
-  preview: PreviewOutput | null,
+export type NormalStepRunnerResult<Output extends StepDataTypeInstance> = BaseRunnerResult & {
   output: Output | null,
-  validationErrors: StepValidationError[],
-}>
+}
 
 export function toNormalStepRunnerResult<
   Output extends StepDataTypeInstance
 >(result: NormalStepRunnerOutput<Output>): NormalStepRunnerResult<Output> {
   return {
-    output: copyStepDataOrNull(result?.output ?? null) ?? null,
+    output: copyStepDataOrNull(result?.output ?? null),
     preview: result?.preview ?? null,
     validationErrors: result?.validationErrors ?? [],
   }
@@ -75,8 +75,6 @@ export function toForkStepRunnerResult<
   }
 }
 
-export type ForkStepRunnerResult<Output extends StepDataTypeInstance> = Narrows<StepRunnerResult, {
-  preview: PreviewOutput,
-  validationErrors: StepValidationError[],
-  output: Output[],
-}>
+export type ForkStepRunnerResult<Output extends StepDataTypeInstance> = BaseRunnerResult & {
+  output: Output[] | null,
+}
