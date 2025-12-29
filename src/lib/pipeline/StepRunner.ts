da2b36b54c47421922f9b4f216ed212b56e8dc84
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-12-29 13:08:19 -0500

    working4

--- src/lib/pipeline/StepRunner.ts
+++ src/lib/pipeline/StepRunner.ts
@@ -1,16 +1,29 @@
 import type { StepDataTypeInstance } from '../../steps.ts'
 import { StepValidationError } from '../errors.ts'
 import { copyStepDataOrNull } from '../step-data-types/_step-data-type-helpers.ts'
-import type { MaybePromise } from '../util/misc.ts'
-import type { AnyStepContext } from './Step.ts'
+import type { MaybePromise, Narrows } from '../util/misc.ts'
+import type { AnyStep, AnyStepContext } from './Step.ts'
 
-export type StepRunner<T extends AnyStepContext> = ({ config, inputData }: {
+export type PreviewOutput = ImageData | ImageData[] | null
+
+export type StepRunner<T extends AnyStepContext> =
+  | NormalStepRunner<T>
+  | ForkStepRunner<T>
+
+export type StepRunnerResult = {
+  preview: AnyStep['outputPreview'],
+  output: AnyStep['outputData'],
+  validationErrors: AnyStep['validationErrors']
+}
+
+export type NormalStepRunner<T extends AnyStepContext> = ({ config, inputData }: {
   config: T['RC'],
   inputData: T['Input'] | null,
 }) => MaybePromise<
-  StepRunnerOutput<T['Output']>
+  NormalStepRunnerOutput<T['Output']>
 >
-export type StepRunnerOutput<Output> =
+
+export type NormalStepRunnerOutput<Output> =
   | null
   | undefined
   | {
@@ -18,15 +31,23 @@ export type StepRunnerOutput<Output> =
   output?: Output | null,
   validationErrors?: StepValidationError[]
 }
-type RunnerPreviewOutput = ImageData | ImageData[] | null
-export type ForkStepRunnerOutput<Output> =
-  | null
-  | undefined
-  | {
-  preview?: RunnerPreviewOutput,
-  branchesOutput?: Output[] | null,
-  validationErrors?: StepValidationError[]
+
+export type NormalStepRunnerResult<Output extends StepDataTypeInstance> = Narrows<StepRunnerResult, {
+  preview: PreviewOutput | null,
+  output: Output | null,
+  validationErrors: StepValidationError[],
+}>
+
+export function toNormalStepRunnerResult<
+  Output extends StepDataTypeInstance
+>(result: NormalStepRunnerOutput<Output>): NormalStepRunnerResult<Output> {
+  return {
+    output: copyStepDataOrNull(result?.output ?? null) ?? null,
+    preview: result?.preview ?? null,
+    validationErrors: result?.validationErrors ?? [],
+  }
 }
+
 export type ForkStepRunner<T extends AnyStepContext> = ({ config, inputData, branchCount }: {
   config: T['RC'],
   inputData: T['Input'] | null,
@@ -35,28 +56,27 @@ export type ForkStepRunner<T extends AnyStepContext> = ({ config, inputData, bra
   ForkStepRunnerOutput<T['Output']>
 >
 
-export function parseStepRunnerResult<Output extends StepDataTypeInstance>(
-  result: StepRunnerOutput<Output>,
-): {
-  preview: ImageData | null,
-  validationErrors: StepValidationError[],
-  outputData: Output | null,
-} {
+export type ForkStepRunnerOutput<Output> =
+  | null
+  | undefined
+  | {
+  preview?: PreviewOutput,
+  branchesOutput?: Output[] | null,
+  validationErrors?: StepValidationError[]
+}
+
+export function toForkStepRunnerResult<
+  Output extends StepDataTypeInstance
+>(result: ForkStepRunnerOutput<Output>): ForkStepRunnerResult<Output> {
   return {
-    outputData: copyStepDataOrNull(result?.output ?? null) ?? null,
+    output: result?.branchesOutput?.map(copyStepDataOrNull) ?? [],
     preview: result?.preview ?? null,
     validationErrors: result?.validationErrors ?? [],
   }
 }
 
-export function parseForkStepRunnerResult<Output extends StepDataTypeInstance>(result: ForkStepRunnerOutput<Output>): {
-  preview: RunnerPreviewOutput,
+export type ForkStepRunnerResult<Output extends StepDataTypeInstance> = Narrows<StepRunnerResult, {
+  preview: PreviewOutput,
   validationErrors: StepValidationError[],
-  outputData: Output[],
-} {
-  return {
-    outputData: result?.branchesOutput?.map(copyStepDataOrNull) ?? [],
-    preview: result?.preview ?? null,
-    validationErrors: result?.validationErrors ?? [],
-  }
-}
+  output: Output[],
+}>
