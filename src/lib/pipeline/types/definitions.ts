ee4d4ebe4e367d21540c0938bd14eba356cfdfb3
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-01-12 20:45:58 -0500

    progress

--- src/lib/pipeline/types/definitions.ts
+++ src/lib/pipeline/types/definitions.ts
@@ -1,6 +1,5 @@
 import type { Component } from 'vue'
 import type { NodeDataType } from '../../node-data-types/_node-data-types.ts'
-import { PassThrough } from '../../node-data-types/PassThrough.ts'
 import { type NodeDef, NodeType } from '../_types.ts'
 
 export const defineStep = <M extends AnyStepMeta<any, any>>(meta: M): M => meta
@@ -34,7 +33,7 @@ export type AnyForkMeta<
   I extends readonly NodeDataType[] = readonly NodeDataType[],
   O extends NodeDataType = NodeDataType
 > = NodeMetaBase & ForkSpecific & IO<I, O> & {
-  branchDefs: readonly string[]
+  branchDefs: string[]
 }
 
 export type AnyBranchMeta<
@@ -78,7 +77,7 @@ export type AnyForkDefinition<
   I extends readonly NodeDataType[] = readonly NodeDataType[],
   O extends NodeDataType = NodeDataType
 > = NodeDefinitionBase & Readonly<ForkSpecific> & IO<I, O> & {
-  branchDefs: readonly NodeDef[]
+  readonly branchDefs: readonly NodeDef[]
 }
 
 export type AnyBranchDefinition<
@@ -107,25 +106,3 @@ type IO<
   I extends readonly NodeDataType[] = readonly NodeDataType[],
   O extends NodeDataType = NodeDataType
 > = PassthroughIO | NormalIO<I, O>
-
-export type EffectiveInputConstructors<M extends NodeMeta<any, any>> =
-// force distribution over union
-  M extends unknown
-    ? // first, handle passthrough
-    M extends { passthrough: true }
-      ? readonly [typeof PassThrough]
-      : // otherwise, just use the StepMeta generic
-      M extends NodeMeta<infer I, any>
-        ? I
-        : never
-    : never
-
-export type EffectiveOutputConstructor<M extends NodeMeta<any, any>> =
-// force distribution over union
-  M extends unknown
-    ? M extends { passthrough: true }
-      ? typeof PassThrough
-      : M extends NodeMeta<any, infer O>
-        ? O
-        : never
-    : never
