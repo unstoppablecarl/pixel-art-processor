8a92dfde036fe77dc273fb3938734e7096d1d6b1
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-01-15 17:19:29 -0500

    progress

--- src/lib/util/color.ts
+++ src/lib/util/color.ts
@@ -1,11 +1,6 @@
 import tinycolor from 'tinycolor2'
 import type { RGBA } from './ImageData.ts'
 
-export const parseColor = (color: string) => {
-  const result = tinycolor(color)
-  return result.toRgb()
-}
-
 export const parseColorData = (color: string): RGBA => {
   const result = tinycolor(color)
   const { r, g, b, a } = result.toRgb()
