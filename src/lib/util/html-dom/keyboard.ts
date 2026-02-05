bb3ab6cb553ac7499ede5d2892e3a754e083bbd9
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-02-05 18:35:42 -0500

    progress

--- src/lib/util/html-dom/keyboard.ts
+++ src/lib/util/html-dom/keyboard.ts
@@ -1,34 +1,6 @@
-import type {
-  LocalToolContext,
-  ToolInputBindings,
-} from '../../../components/CanvasEditor/TileGridEdit/_tile-grid-editor-types.ts'
 import { tinykeys } from './tinykey.ts'
 
 export type InputBindings = Record<string, (event: KeyboardEvent) => void>
-
-export function makeCopyPasteKeys<T>(
-  copy: (local: LocalToolContext<T>) => void,
-  paste: (local: LocalToolContext<T>) => void,
-): ToolInputBindings<T> {
-  return {
-    'Control+c': (local, e) => {
-      e.preventDefault()
-      copy(local)
-    },
-    'Meta+c': (local, e) => {
-      e.preventDefault()
-      copy(local)
-    },
-    'Control+v': (local, e) => {
-      e.preventDefault()
-      paste(local)
-    },
-    'Meta+v': (local, e) => {
-      e.preventDefault()
-      paste(local)
-    },
-  }
-}
 
 export function bindInputKey(bindings: InputBindings, target: Window | HTMLElement = window) {
   const unsubscribe = tinykeys(target, bindings)
@@ -39,3 +11,30 @@ export function bindInputKey(bindings: InputBindings, target: Window | HTMLEleme
     removed = true
   }
 }
+
+function makeKeyListener() {
+  const pressedKeys = new Set()
+  window.addEventListener('keydown', (event) => {
+    if (pressedKeys.has(event.key)) return
+    pressedKeys.add(event.key)
+  })
+
+  window.addEventListener('keyup', (event) => {
+    pressedKeys.delete(event.key)
+  })
+
+  window.addEventListener('blur', () => {
+    pressedKeys.clear()
+  })
+
+  return {
+    isDown: (keyCode: string) => pressedKeys.has(keyCode),
+    controlIsDown: () => pressedKeys.has('Control'),
+    shiftIsDown: () => pressedKeys.has('Shift'),
+    altIsDown: () => pressedKeys.has('Alt'),
+    metaIsDown: () => pressedKeys.has('Meta'),
+  }
+}
+
+export const KEY_INPUT = makeKeyListener()
+
