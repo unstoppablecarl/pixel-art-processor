ff480b7376b4ed40c4723450c2b570d53a07d4ae
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-04-09 14:43:11 -0400

    progress

--- src/components/CanvasEditor/CanvasPaint/CanvasRenderer.ts
+++ src/components/CanvasEditor/CanvasPaint/CanvasRenderer.ts
@@ -1,5 +1,6 @@
+import { makeRenderQueue } from 'pixel-data-js'
 import { makePixelCanvas, type PixelCanvas } from '../../../lib/util/html-dom/PixelCanvas.ts'
-import { makeCanvasFrameRenderer, makeRenderQueue } from '../../../lib/util/html-dom/renderCanvasFrame.ts'
+import { makeCanvasFrameRenderer } from '../../../lib/util/html-dom/renderCanvasFrame.ts'
 import { type PixelGridLineRenderer } from '../_core/renderers/PixelGridLineRenderer.ts'
 import type { CanvasPaintEditorState } from './CanvasPaintEditorState.ts'
 import type { CanvasPaintToolset } from './CanvasPaintToolset.ts'
--- src/components/CanvasEditor/TileGridEdit/renderers/TileRenderer.ts
+++ src/components/CanvasEditor/TileGridEdit/renderers/TileRenderer.ts
@@ -1,6 +1,6 @@
-import { makePixelData, setPixelData } from 'pixel-data-js'
+import { makePixelData, makeRenderQueue, setPixelData } from 'pixel-data-js'
 import { drawText, makePixelCanvas } from '../../../../lib/util/html-dom/PixelCanvas.ts'
-import { makeCanvasFrameRenderer, makeRenderQueue } from '../../../../lib/util/html-dom/renderCanvasFrame.ts'
+import { makeCanvasFrameRenderer } from '../../../../lib/util/html-dom/renderCanvasFrame.ts'
 import type { TileId } from '../../../../lib/wang-tiles/WangTileset.ts'
 import type { PixelGridLineRenderer } from '../../_core/renderers/PixelGridLineRenderer.ts'
 import { makeSingleTileSync } from '../data/TileSync.ts'
