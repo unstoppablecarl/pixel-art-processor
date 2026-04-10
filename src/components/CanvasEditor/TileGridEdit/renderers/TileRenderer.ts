2f25286fa22c95e7a215cbe473781bf50282a023
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-04-10 15:30:31 -0400

    progress

--- src/components/CanvasEditor/TileGridEdit/renderers/TileRenderer.ts
+++ src/components/CanvasEditor/TileGridEdit/renderers/TileRenderer.ts
@@ -52,8 +52,8 @@ export function makeTileRenderer(
       state.scale,
       () => pixelData.imageData,
       (ctx) => {
-        tileGridEdgeColorRenderer.drawTileEdges(ctx, tileId)
         toolset.currentToolHandler.tilePixelOverlayDraw?.(ctx, tileId)
+        tileGridEdgeColorRenderer.drawTileEdges(ctx, tileId)
       },
       (ctx) => {
         if (state.shouldDrawGrid()) {
